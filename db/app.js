f696df8aab6e5fe4f22504fe21da34f401a75a8a
AkivaKn <akivakaufman@gmail.com>
2024-04-15 14:39:39 +0100

    Refactor to seperate error handling

--- db/app.js
+++ db/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {getTopics} = require('./controllers/topics-controller')
 const { getApis } = require('./controllers/api-controller')
 const { getArticleById } = require('./controllers/articles-controller')
+const { customErrors, psqlErrors, serverError } = require('./error-handling')
 
 const app = express()
 
@@ -15,23 +16,11 @@ app.get('/api/articles/:article_id', getArticleById)
 
 app.all('/*',(req,res,next)=> next({status:404,msg:'Path not found'}))
 
-app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({msg:err.msg})
-    } 
-    next(err)
-})
+app.use(customErrors)
 
-app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-        res.status(400).send({msg:'Bad request'})
-    }
-    next(err)
-})
+app.use(psqlErrors)
 
-app.use((err, req, res, next) => {
-    res.status(500).send({msg:'Internal server error'})
-})
+app.use(serverError)
 
 module.exports = app
 
---
+++ db/error-handling.js
@@ -0,0 +1,19 @@
+exports.customErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+      res.status(err.status).send({ msg: err.msg });
+    } else {
+        next(err);
+    }
+}
+  
+exports.psqlErrors = (err, req, res, next) => {
+    if (err.code === "22P02") {
+      res.status(400).send({ msg: "Bad request" });
+    } else {
+        next(err);
+    }
+}
+  
+exports.serverError = (err, req, res, next) => {
+    res.status(500).send({ msg: "Internal server error" });
+  }
