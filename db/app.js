aae72a767d46a88cf94cacfa851150432b937020
AkivaKn <akivakaufman@gmail.com>
2024-04-15 14:20:50 +0100

    Create GET /api/articles endpoint
    Build endpoint using TDD and update endpoints.json
    No error handling necessary in this instance.

--- db/app.js
+++ db/app.js
@@ -1,37 +1,41 @@
-const express = require('express')
-const {getTopics} = require('./controllers/topics-controller')
-const { getApis } = require('./controllers/api-controller')
-const { getArticleById } = require('./controllers/articles-controller')
+const express = require("express");
+const { getTopics } = require("./controllers/topics-controller");
+const { getApis } = require("./controllers/api-controller");
+const {
+  getArticleById,
+  getArticles,
+} = require("./controllers/articles-controller");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/topics', getTopics)
+app.get("/api/topics", getTopics);
 
-app.get('/api', getApis)
+app.get("/api", getApis);
 
-app.get('/api/articles/:article_id', getArticleById)
+app.get("/api/articles", getArticles);
 
-app.all('/*',(req,res,next)=> next({status:404,msg:'Path not found'}))
+app.get("/api/articles/:article_id", getArticleById);
 
-app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({msg:err.msg})
-    } 
-    next(err)
-})
+app.all("/*", (req, res, next) => next({ status: 404, msg: "Path not found" }));
 
 app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
-        res.status(400).send({msg:'Bad request'})
-    }
-    next(err)
-})
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({msg:'Internal server error'})
-})
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  }
+  next(err);
+});
 
-module.exports = app
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Internal server error" });
+});
 
+module.exports = app;
--- db/controllers/articles-controller.js
+++ db/controllers/articles-controller.js
@@ -1,10 +1,19 @@
-const { selectArticleById } = require("../models/articles-model")
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articles-model");
 
 exports.getArticleById = (req, res, next) => {
-    const {article_id} = req.params
-    return selectArticleById(article_id)
-        .then((article) => {
-        res.status(200).send({article})
-        })
-    .catch(next)
-}
+  const { article_id } = req.params;
+  return selectArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.getArticles = (req, res, next) => {
+  return selectArticles().then((articles) => {
+    res.status(200).send({ articles });
+  });
+};
