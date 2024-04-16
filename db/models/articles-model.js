604cf82020ebf21187ed0b146dc35903138468fd
AkivaKn <akivakaufman@gmail.com>
2024-04-16 12:23:02 +0100

    Refactor updateArticle according to feedback

--- db/models/articles-model.js
+++ db/models/articles-model.js
@@ -31,14 +31,17 @@ exports.selectArticles = () => {
 };
 
 exports.updateArticle = (article_id, inc_votes) => {
-  return this.selectArticleById(article_id)
-    .then(() => {
-      return db.query(`UPDATE articles 
+  return db
+    .query(
+      `UPDATE articles 
     SET votes=votes+$1
-    WHERE article_id = $2 RETURNING *;`, [inc_votes, article_id])
-    
-  })
+    WHERE article_id = $2 RETURNING *;`,
+      [inc_votes, article_id]
+    )
     .then(({ rows }) => {
-   return rows[0]
-    })
-}
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+      return rows[0];
+    });
+};
