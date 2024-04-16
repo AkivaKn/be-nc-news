83ff606781f481ade066cf07c4db58cb87594977
AkivaKn <akivakaufman@gmail.com>
2024-04-16 15:29:27 +0100

    Task 12. Add comment_count to GET article
    Using TDD, add comment_count to GET /api/articles/
    :article_id

--- db/models/articles-model.js
+++ db/models/articles-model.js
@@ -1,13 +1,20 @@
 const db = require("../connection");
-const { checkTopicExists } = require("./topics-model");
 
 exports.selectArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1;`, [article_id])
+    .query(`SELECT articles.author,title,articles.article_id,articles.body,topic,articles.created_at,articles.votes,article_img_url, COUNT(comment_id) AS comment_count 
+    FROM articles 
+    LEFT JOIN comments 
+    ON articles.article_id = comments.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY 
+    articles.article_id;
+    `, [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
       }
+        rows[0].comment_count = Number(rows[0].comment_count);
       return rows[0];
     });
 };
@@ -49,3 +56,4 @@ exports.updateArticle = (article_id, inc_votes) => {
       return rows[0];
     });
 };
+
