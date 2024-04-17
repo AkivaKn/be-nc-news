9279089e8ed587ec2d78790facce78b2499a0a01
AkivaKn <akivakaufman@gmail.com>
2024-04-17 11:01:54 +0100

    Task 16. Express Routers
    Refactor app to use routers and sub routers

--- db/app.js
+++ db/app.js
@@ -1,40 +1,12 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics-controller");
-const { getApis } = require("./controllers/api-controller");
-const {
-  getArticleById,
-  getArticles,
-  patchArticle,
-} = require("./controllers/articles-controller");
 const { customErrors, psqlErrors, serverError } = require("./error-handling");
-const {
-  getComments,
-  postComment,
-  deleteComment,
-} = require("./controllers/comments-controller");
-const { getUsers } = require("./controllers/users-controller");
+const apiRouter = require("./routes/api-router");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/topics", getTopics);
-
-app.get("/api", getApis);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles/:article_id/comments", getComments);
-
-app.get("/api/users", getUsers);
-
-app.post("/api/articles/:article_id/comments", postComment);
-
-app.patch("/api/articles/:article_id", patchArticle);
-
-app.delete("/api/comments/:comment_id", deleteComment);
+app.use("/api", apiRouter);
 
 app.all("/*", (req, res, next) => next({ status: 404, msg: "Path not found" }));
 
